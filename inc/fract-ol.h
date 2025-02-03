9fdde87aef3133414635e30d97ed588566895f87
David Labrador Gracia <dalabrad@c4r7s5.42urduliz.com>
2025-02-03 16:06:06 +0100

    added inc/fract-ol.h

---
+++ inc/fract-ol.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fract-ol.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dalabrad <dalabrad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/03 15:43:22 by dalabrad          #+#    #+#             */
+/*   Updated: 2025/02/03 15:58:46 by dalabrad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+# include <mlx.h>
+# include <X11/keysym.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <math.h>
+# include "../libft/libft.h"
+
+# define MLX_ERROR 1
+# define MALLOC_ERROR 2
+
+
+#endif
