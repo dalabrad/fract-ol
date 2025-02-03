7bfe7fff95f4975e33178a837382d64c35215ead
David Labrador Gracia <dalabrad@c4r7s5.42urduliz.com>
2025-02-03 16:17:53 +0100

    modified main.c

--- main.c
+++ main.c
@@ -6,20 +6,21 @@
 /*   By: dalabrad <dalabrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 13:49:17 by dalabrad          #+#    #+#             */
-/*   Updated: 2025/02/03 16:05:09 by dalabrad         ###   ########.fr       */
+/*   Updated: 2025/02/03 16:16:55 by dalabrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "inc/fract-ol.h"
+#include "inc/fractol.h"
 
-int	main(int argc ,char *argv[])
+int	main(int argc, char *argv[])
 {
-	if ((argc == 2 && !ft_strncmp(argv[1], "Mandelbrot", 11)) ||
-			(argc == 4 && !ft_strncmp(argv[1], "Julia", 6)))
+	if ((argc == 2 && !ft_strncmp(argv[1], "Mandelbrot", 11))
+		|| (argc == 4 && !ft_strncmp(argv[1], "Julia", 6)))
 		ft_printf("Everything ok!!\n");
 	else
 	{
-		ft_printf("Error! Please enter:\n./fractol mandelbrot\nor\n./fractol julia <real part> <imaginary part>\n");
+		ft_printf("Error! Please enter:\n./fractol mandelbrot\nor\n");
+		ft_printf("./fractol julia <real part> <imaginary part>\n");
 		return (1);
 	}
 	return (0);
