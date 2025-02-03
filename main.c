5aa1bb85f8c583339a5614aa432de38679f95acf
David Labrador Gracia <dalabrad@c4r7s5.42urduliz.com>
2025-02-03 16:26:48 +0100

    modified main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: dalabrad <dalabrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 13:49:17 by dalabrad          #+#    #+#             */
-/*   Updated: 2025/02/03 16:16:55 by dalabrad         ###   ########.fr       */
+/*   Updated: 2025/02/03 16:25:36 by dalabrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ int	main(int argc, char *argv[])
 		ft_printf("Everything ok!!\n");
 	else
 	{
-		ft_printf("Error! Please enter:\n./fractol mandelbrot\nor\n");
-		ft_printf("./fractol julia <real part> <imaginary part>\n");
+		ft_printf("Error! Please enter:\n\t./fractol mandelbrot\nor\n");
+		ft_printf("\t./fractol julia <real part> <imaginary part>\n");
 		return (1);
 	}
 	return (0);
