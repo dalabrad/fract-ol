27dea3397c5a9e6c0adba0199cf73e0d5a1e6384
David <dalabrad@student.42urduliz.com>
2025-02-04 10:11:34 +0100

    modified inc/fractol.h

--- inc/fractol.h
+++ inc/fractol.h
@@ -6,22 +6,26 @@
 /*   By: dalabrad <dalabrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 15:43:22 by dalabrad          #+#    #+#             */
-/*   Updated: 2025/02/03 16:15:54 by dalabrad         ###   ########.fr       */
+/*   Updated: 2025/02/04 10:07:17 by dalabrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACTOL_H
 # define FRACTOL_H
 
-# include <mlx.h>
 # include <X11/keysym.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
 # include <math.h>
+# include "../minilibx-linux/mlx.h"
 # include "../libft/libft.h"
 
-# define MLX_ERROR 1
-# define MALLOC_ERROR 2
+# define INPUT_ERROR 1
+# define MLX_ERROR 2
+# define MALLOC_ERROR 3
+
+//	fractol_error_msg.c
+void	error_msg(int err_id);
 
 #endif
