eef487813e59407d718a699622d330a234edfb4e
David <dalabrad@student.42urduliz.com>
2025-02-05 20:36:07 +0100

    modified main.c adding two functions for the implementation of the Julia fractal

--- main.c
+++ main.c
@@ -6,11 +6,63 @@
 /*   By: dalabrad <dalabrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 13:49:17 by dalabrad          #+#    #+#             */
-/*   Updated: 2025/02/05 12:14:05 by dalabrad         ###   ########.fr       */
+/*   Updated: 2025/02/05 20:34:56 by dalabrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "inc/fractol.h"
+
+static int	check_argument(char *str)
+{
+	int	dot_count;
+
+	if (*str == '-')
+		str++;
+	if (*str == '.')
+		return (0);
+	dot_count = 0;
+	while (*str)
+	{
+		if (((*str < '0' || *str > '9') && *str != '.' )
+			|| (*str == '.' && dot_count == 1))
+			return (0);
+		else if (*str == '.' && !*(str + 1))
+			return (0);
+		else if (*str == '.' && dot_count == 0)
+			dot_count = 1;
+		str++;
+	}
+	return (1);
+}
+
+static double	atod(char *str)
+{
+	double	result;
+	int		sign;
+	int		i;
+
+	sign = 1;
+	result = 0.0;
+	if (*str == '-')
+	{
+		sign = -1;
+		str++;
+	}
+	while (*str && *str != '.')
+	{
+		result = result * 10 + (*str - '0');
+		str++;
+	}
+	if (*str == '.')
+		str++;
+	i = 1;
+	while (*str)
+	{
+		result += (*str - '0') / pow(10.0, (double)i++);
+		str++;
+	}
+	return (sign * result);
+}
 
 int	main(int argc, char *argv[])
 {
